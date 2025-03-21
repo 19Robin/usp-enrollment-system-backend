143754ade4c284e9290f2470bfb90a17c53a500b
priya <s11197877@gmail.com>
2025-03-21 17:47:14 +1200

    reflecting change in db.js

--- Model/studentModel.js
+++ Model/studentModel.js
@@ -1,7 +1,5 @@
-
 // filepath: c:\Users\slade\Downloads\CS415\Assignment 1\usp-enrollment-system-backend\Model\userModel.js
 const {db} = require('../db');
-
 
 const getStudentById = (studentId, callback) => {
   const query = 'SELECT * FROM students WHERE student_id = ?';
